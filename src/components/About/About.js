46f69a3678a8925798906a6a80a813ba65633919
Trevor Ramey <treramey@gmail.com>
2020-03-31 14:48:37 -0500

    finnied react 4 afternoon project

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import History from "../History/History";
+import Contact from "../Contact/Contact";
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        <div className='subnav'>
+        <div className="subnav">
+          <Link to="/about" className="subnav_links">
+            <h3>About</h3>
+          </Link>
+          <Link to="/about/history" className="subnav_links">
+            <h3>History</h3>
+          </Link>
+          <Link to="/about/contact" className="subnav_links">
+            <h3>Contact</h3>
+          </Link>
         </div>
-        <div className='box'>
+        <div className="box">
+          <Switch>
+            <Route path="/about/history" component={History} />
+            <Route path="/about/contact" component={Contact} />
+            <Route
+              path="/about"
+              render={() => (
+                <div>
+                  <h1>About the University</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent varius lobortis blandit. Nulla sollicitudin ante
+                    orci, nec placerat dolor hendrerit vel. Vivamus justo
+                    ligula, egestas et mauris nec, posuere finibus orci. Quisque
+                    nec velit purus. Nunc sit amet risus et purus rhoncus
+                    aliquet ac at tellus. Fusce pretium elit ut odio lobortis
+                    pharetra. Vivamus gravida lacus ut erat accumsan, nec
+                    hendrerit tortor fringilla.
+                  </p>
+                </div>
+              )}
+            />
+          </Switch>
         </div>
       </div>
-    )
+    );
   }
 }
--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,18 +1,43 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+// eslint-disable-next-line
 export default class ClassList extends Component {
   constructor() {
-    super()
-    
+    super();
+
+    this.state = {
+      strudents: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        `http://localhost:3005/students?class=${this.props.match.params.class}`
+      )
+      .then(response => {
+        this.setState({
+          strudents: response.data
+        });
+      });
   }
 
   render() {
+    const students = this.state.strudents.map((el, i) => (
+      <Link to={`/student/${el.id}`} key={i}>
+        <h3 key={i}>
+          {el.first_name}
+          {el.last_name}
+        </h3>
+      </Link>
+    ));
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {students}
       </div>
-    )
+    );
   }
 }
